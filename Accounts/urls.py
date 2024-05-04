91169ee046da13e05e2b8baf0eb7a4b8a962bc11
dipanshu0511vora <dipanshuvora1@gmail.com>
2024-05-04 19:53:42 +0100

    Gmail 2 step Authentication

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from Accounts.views import login_page,register_page
+from Accounts.views import login_page,register_page, activate_email
 
 
 urlpatterns = [
    path('login/', login_page , name="login"),
    path('register/' , register_page , name="register"),
+   path('activate/<email_token>/' , activate_email , name="activate_email"),
+
    
 
 ]
